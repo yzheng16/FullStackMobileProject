fc09c9d0b21841920497ba57848fb05c1f19878b
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-07-20 17:49:01 -0600

    Added REST API
    1.  Installed Sails.js
    2. Installed Nodemon for hot reload
    3. Installed Sails Morgan for printing respone and endpoint
    4. Installed Postman for triggering Post request
    5. Used Sails.js waterline functions

---
+++ MobileApp/api/controllers/PostsController.js
@@ -0,0 +1,58 @@
+module.exports = {
+    posts: async function(req, res) {
+
+        //Original function
+        try {
+            const posts = await Post.find()
+            res.send(posts)
+        } catch (err) {
+            res.serverError(err.toString())
+        }
+
+        //Sails waterline function
+        // Post.find().exec(function(err, posts) {
+        //     if(err) {
+        //         return res.serverError(err.toString())
+        //     }
+        //     res.send(posts)
+        // })
+    },
+
+    findById: function(req, res) {
+        const postId = req.param('postId')
+
+        Post.findOne({id: postId}).exec(function(err, post) {
+            if(err) {
+                return res.serverError(err.toString())
+            }
+            if(!post) {
+                res.send("Could not find post id = " + postId)
+            }else {
+                res.send(post)
+            }
+        })
+    },
+
+    create: function(req, res) {
+        const title = req.body.title
+        const postBody = req.body.postBody
+
+        sails.log.debug('My title: ' + title)
+        sails.log.debug('Body: ' + postBody)
+        
+        Post.create({title: title, body: postBody}).exec(function(err) {
+            if(err) {
+                return res.serverError(err.toString())
+            }
+            console.log("Finished creating post object")
+            return res.end()
+        })
+    },
+
+    delete: async function(req, res) {
+        const postId = req.param('postId')
+
+        await Post.destroy({id: postId})
+        res.send("Finished deleting post")
+    }
+}
